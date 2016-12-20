0969ae680c54c0cf7e1bbc916b09501613857f5e
DevinThePancake <DevinThePancake@users.noreply.github.com>
2016-12-20 14:13:56 -0800

    Eval command

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var prefix = "bing, "
+var admins = ['128942674722947072', '259044020926414848'];
 
 bot.on("message", msg => {
 	if(msg.author.bot) return;
@@ -18,6 +19,30 @@ else
 else
 	if(msg.content.startsWith(prefix + "kys")) {
 	msg.channel.sendMessage("https://suicidepreventionlifeline.org/");
+} 
+	else
+if (message.content.startsWith(prefix + 'eval') && admins.includes(message.author.id)) {
+   evaluateString(message);
+   return;
+}//IN the client.on(message)
+});
+function evaluateString(message){
+  if (!admins.includes(message.author.id)) {
+  console.log(" WARN ") + (" Somehow an unauthorized user got into eval!");
+  return;
+  }
+  let result = 'u13g5u35y7jg73';
+  console.log("Running eval");
+  try {
+  result = eval(message.content.substring(6));
+  } catch (e) {
+  console.log(" ERROR ") + (" " + e);
+  message.channel.sendMessage("```diff\n- " + e + "```");
+  }
+  if (result !== 'u13g5u35y7jg73') {
+  message.channel.sendMessage( "**Output:**```js\n" + result + "```");
+  console.log("Output: " + result);
+ }
 }
 });
 
